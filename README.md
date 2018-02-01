7b9f220aa8a58680374395b31bfcd9c9b17d5758
Nikita Burmistrov <nikita.tnv@yandex.ru>
2018-02-01 17:38:37 +0300

    repeated actions from video Spring Security, MD

---
+++ README.md
@@ -0,0 +1,7 @@
+#Project to study Spring
+##from [khasang.io](https://khasang.io/ "Сайт Khasang")
+
+http://intellitech.pro/wp-content/uploads/2017/06/ff.png
+
+![Spring logo](http://intellitech.pro/wp-content/uploads/2017/06/ff.png "Spring logo")
+
--- src/main/java/io/khasang/eshop/controller/AppController.java
+++ src/main/java/io/khasang/eshop/controller/AppController.java
@@ -2,9 +2,12 @@ package io.khasang.eshop.controller;
 
 import io.khasang.eshop.model.CreateTable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -25,6 +28,22 @@ public class AppController {
     @ResponseBody
     public String createTable() {
         return createTable.createTableStatus();
+    }
+
+    @RequestMapping("/admin/page")
+    public String getSecurePage() {
+        return "admin";
+    }
+
+    @RequestMapping("/user/**")
+    public String getUserPage() {
+        return "user";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = {"/password/{password}"}, method = RequestMethod.GET)
+    public String getEncryptedPassword(@PathVariable("password") String password) {
+        return new BCryptPasswordEncoder().encode(password);
     }
 
 }
